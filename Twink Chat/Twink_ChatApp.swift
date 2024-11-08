3427117515117d4af2ad37457cef3fcccc566a4d
Mohammed Selim <mo.selim.dev@gmail.com>
2024-11-09 00:32:07 +0200

    Initial Commit

---
+++ Twink Chat/Twink_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twink_ChatApp.swift
+//  Twink Chat
+//
+//  Created by Mohamed Selim on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Twink_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
